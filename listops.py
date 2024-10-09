0f4af123fd8ccdfbeb7eb5b20fcf81d72b985ffb
insomnes <denginm@gmail.com>
2024-10-09 22:33:45 +0200

    Speed up remove duplicates

--- listops.py
+++ listops.py
@@ -1,14 +1,5 @@
 def remove_duplicates(lst: list) -> list:
-    present = set()
-    result = []
-    for item in lst:
-        if item in present:
-            continue
-        present.add(item)
-        result.append(item)
-
-    return result
-
+    return list(set(lst))
 
 def add_elements(lst1: list, lst2: list) -> list:
     return lst1 + lst2
