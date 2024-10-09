1ee708cc82e69a4393583f960e70eca3ac7eba4d
insomnes <denginm@gmail.com>
2024-10-09 22:26:17 +0200

    Add single element operation add

--- listops.py
+++ listops.py
@@ -14,6 +14,10 @@ def add_elements(lst1: list, lst2: list) -> list:
     return lst1 + lst2
 
 
+def add_element(lst: list, item) -> list:
+    return add_elements(lst, [item])
+
+
 def add_elements_wo_dups(lst1: list, lst2: list) -> list:
     return remove_duplicates(
         add_elements(lst1, lst2)
